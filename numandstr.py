76da8047786d2b5c3171d3379af89c6d8467d02d
DESKTOP-L2TT5UE\k <aa007878@naver.com>
2023-01-08 22:12:47 +0900

    2023-01-08 아마도 마무리?
    학교에서 배운것보다 엄청 많이 나온다. 종종 복습 필수

--- numandstr.py
+++ numandstr.py
@@ -24,3 +24,60 @@ import math
 print(math.pi)
 print(math.ceil(5.1)) 
 print(math.ceil(5)) 
+
+# 문자열 str
+str1 = '123456789 '
+print('len(str1)=',len(str1))
+
+# escape 여러 기능이 있음
+# print('i'm boy') 오류난다! 역슬려쉬 (달려표시) 사용
+print ('I\'m boy')
+print ('a \n b')
+
+# raw string (escape 안하기 위함 )
+raw_s1 = r'd:wer\asdf'
+print(raw_s1)
+
+# 멀티라인
+multi_str = '''
+asdf
+asdf
+asdf
+'''
+print (multi_str)
+multi_str = \
+'''
+asdf
+asdf
+asdf
+'''
+print (multi_str)
+multi_str = \
+'asdf'\
+'asdf'\
+'asdf'
+print (multi_str)
+
+#문자열 연산
+str_o1 = '123'
+str_o2 = 'adsf'
+str_o3 = 'a1s2'
+str_o4 = 'a1s1d213'
+print(str_o1+str_o2)
+print('1' in str_o1)
+print('1' not in str_o1)
+
+print('Capitalize',str_o2.capitalize())
+print('endswith?:',str_o2.endswith('f'))
+print('endswith?:',str_o2.endswith('1'))
+print("replace",str_o2.replace('dsf','repllllace') )
+print("sorted",sorted(str_o2))
+print("split",str_o4.split('1'))
+
+#sqence
+im_str = "hello world"
+
+print(dir(im_str)) #__iter__ 가 있으면 반복 가능
+
+for i in im_str:
+    print(i)
