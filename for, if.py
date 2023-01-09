bffc0403db6cc0b69fa4e1737ec45b6043416a21
DESKTOP-L2TT5UE\k <aa007878@naver.com>
2023-01-09 21:46:02 +0900

    for if
    거의 비슷하다 학교에서 배운 것이랑

---
+++ for, if.py
@@ -0,0 +1,23 @@
+#iterable 리턴함수 : range, reversed, enumerate, filter, map, zip
+
+names = ['kim', 'park', 'cho', 'lee'] # list
+for i in names :
+    print('names :',i)
+
+lotter_numbers = [11, 13, 16,25,36,44]
+for i in lotter_numbers:
+    print('numbers =',i)
+
+my_info = {
+    'name': 'lee',
+    'age' : '33',
+    'city' : 'seoul'
+} # dict
+for i in my_info:
+    print(i , end =' : ' )
+    print( my_info[i])
+
+# a.isupper() > a가 대문자인가 , a.upper() a를 대문자로
+
+
+
