c8ef3b305ce64f35b71b25294323724b16a2ffa8
DESKTOP-L2TT5UE\k <aa007878@naver.com>
2023-01-13 17:21:39 +0900

    예외 처리하기
    예외로 보내버리는 방식도 있는걸 배웟다.
    대문자 항상 조심하기

--- exception.py
+++ exception.py
@@ -5,3 +5,56 @@
 # 예외는 던져진다 ?
 # 예외 무시
 
+# SyntaxError ex) print('error)
+
+# NameError ex) a= 10, print(b)
+
+# ZeroDivisionError ex) print(100/0)
+
+# IndexError ex) x=[10,20], print(x[3])
+
+# keyError ex) dic = {'name': 'lee'}, print (dic['age'])  > 예외 해결 print(dic.get('age'))
+
+# AttributeError : 모듈, 클래스에 있는 잘못된 속성 사용 예외
+
+# ValueError ex) x= [10, 20], x.remove(100)
+
+# FileNotFoundError :없는 파일 열기
+
+# TypeError ex) x=[1,2], y=(2,3), print(x+y)
+
+# 예외처리 기본
+# try, except, else, finally
+
+name=['kim','lee','park']
+
+try:
+    z = input('inter your name')
+    x= name.index(z)
+    print('{} Found it {} in name'.format(z, x+1))
+except ValueError as e:
+    print(e)
+    print('Not found it! - occurred ValueError!')
+else :
+    print('Ok! else.')
+finally :
+    print('이건 항상 실행된다')
+
+
+try:
+    if name[2]== 'kim':
+        print('gogo')
+    else:
+        raise ValueError
+except ValueError:
+    print('get out')
+else:
+    print('hello')
+
+
+    
+
+
+
+
+
