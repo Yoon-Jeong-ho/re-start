e04edcca78b8bdc98339da0a3087d55860d779ff
K\aa007 <aa007878@naver.com>
2023-09-05 23:38:53 +0900

    여기선 c를 풀지요~

--- c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
+++ c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
@@ -4,16 +4,8 @@
 #include<string.h>
 
 int main() {
-    char x[6][5] = { {'Z', 'E', 'R', 'O', '-'}, {'O', 'N', 'E', '-', '-'}, {'T', 'W', 'O', '-', '-'}, {'T', 'H', 'R', 'E', 'E'}, {'F', 'O', 'U', 'R', '-'},{'F', 'I', 'V', 'E', '-'} };
-    int a;
-
-    for (int i = 0; i < 3; i++) {
-        scanf("%d", &a);
-        for (int j = 0; j < 5; j++) {
-            printf("%c", x[a][j]);
-        }
-        printf("\n");
-
-    }
-    return 0;x
+    int n;
+    scanf("%d",&n);
+    
+    return 0;
 }   
