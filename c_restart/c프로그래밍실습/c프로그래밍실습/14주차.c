a0f4490f17b972193aeac4c3d39ac14fc35f56ef
K\aa007 <aa007878@naver.com>
2023-09-26 11:22:48 +0900

    개졸리네

--- c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
+++ c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
@@ -4,138 +4,36 @@
 #include<stdlib.h>
 #include<string.h>
 #include<time.h>
-long long int n = 0, H[100000] = { 0 };
-void insertItem(long long int key);
-int removeMax();
-void upHeap(int i);
-void downHeap(int i);
-void printHeap();
-void rBuildHeap(int i);
 int main() {
-    char c;
-    int t;
-    long long int a;
-    scanf("%d", &t);
-    for (int i = 0; i < t; i++) {
-        scanf("%lld", &a); 
-        if (a == 0) {
-            if (n != 0) {
-                printf("%lld\n", removeMax());
-            }
-            else printf("0\n");
-        }
-        else {
-            insertItem(a);
-            
-        }
-    }
+	char c[101],d[100][100],e[100], f[100][100],resultc[100];
+	int n,a=0,b=0,i=0,result = 0,x,y;
+	scanf("%d %d", &x, &y);
+	getchar();
+	gets_s(c, 100, stdin);
+	gets_s(e, 100, stdin);
+	while (c[i] != '\0') {
+		if (c[i] == ' ') { d[a][b] = '\0'; a++; b = 0; }
+		else d[a][b++] = c[i];
+		i++;
+	}
+	d[a][b] = '\0';
+	a = 0; b = 0; i = 0;
+	while (e[i] != '\0') {
+		if (e[i] == ' ') { f[a][b] = '\0'; a++; b = 0; }
+		else f[a][b++] = e[i];
+		i++;
+	}
+	f[a][b] = '\0';
+	if (strcmp(d[x], f[y]) <= 0) {
+		strcpy(resultc, d[x]);
+		resultc[strlen(d[x])] = '\0';
+		strcat(resultc, f[y]);
+	}
+	else {
+		strcpy(resultc, f[y]);
+		resultc[strlen(f[y])] = '\0';
+		strcat(resultc, d[x]);
+	}
 
-    return 0;
-}
-void rBuildHeap(int i) {
-    if (i > n) return;
-    rBuildHeap(i * 2);
-    rBuildHeap(i * 2 + 1);
-    downHeap(i);
-    return;
-}
-void insertItem(long long int key) {
-    H[++n] = key;
-    upHeap(n);
-}
-int removeMax() {
-    int result = H[1];
-    H[1] = H[n--];
-    downHeap(1);
-    return result;
-}
-void upHeap(int i) {
-    int tmp;
-    if (i == 1) return;
-    if (H[i] >= H[i / 2]) return;
-    tmp = H[i];
-    H[i] = H[i / 2];
-    H[i / 2] = tmp;
-    upHeap(i / 2);
-}
-void downHeap(int i) {
-    int tmp;
-    if (i > n / 2) return;
-    if (H[i] <= H[i * 2 + 1] && H[i] <= H[i * 2]) return;
-    if (H[i * 2 + 1] < H[i * 2]) {
-        tmp = H[i * 2 + 1];
-        H[i * 2 + 1] = H[i];
-        H[i] = tmp;
-        return downHeap(i * 2 + 1);
-    }
-    else {
-        tmp = H[i * 2];
-        H[i * 2] = H[i];
-        H[i] = tmp;
-        return downHeap(i * 2);
-    }
-
-}
-void printHeap() {
-    int i = 1;
-    while (i <= n) {
-        printf("%d ", H[i++]);
-    }
-}
-
-
-
-
-
-
-
-int kk = 0, l = 0;
-
-int heap_sort(int A[], int);
-int build_min_heap(int A[], int n);
-int heapify(int A[], int k, int n);
-
-
-int main_heap() {
-    int i, n, * list;
-    scanf("%d %d", &n, &l);
-    list = (int*)malloc(sizeof(int) * (n + 1));
-    for (int j = 1; j <= n; j++) scanf("%d", &list[j]);
-    // 퀵 정렬 수행(left: 배열의 시작 = 0, right: 배열의 끝 = 8)
-    heap_sort(list, n);
-    if (kk < l) printf("-1");
-    //for (int j = 0; j < n; j++)printf("%d", list[j]);
-}
-int heap_sort(int A[], int n) {
-    build_min_heap(A, n);
-    int tmp;
-    for (int i = n; i >= 2; i--) {
-        tmp = A[i];
-        A[i] = A[1];
-        A[1] = tmp;
-        kk++;    if (kk == l) printf("%d %d", A[i], A[1]);
-
-        heapify(A, 1, i - 1);
-
-    }
-}
-int build_min_heap(int A[], int n) {
-    for (int i = n / 2; i >= 1; i--)  heapify(A, i, n);
-}
-int heapify(int A[], int k, int n) {
-    int left = 2 * k; int right = 2 * k + 1, smaller, tmp;
-    if (right <= n) {
-        if (A[left] < A[right]) smaller = left;
-        else smaller = right;
-    }
-    else if (left <= n) smaller = left;
-    else return;
-
-    if (A[smaller] < A[k]) {
-        tmp = A[smaller];
-        A[smaller] = A[k];
-        A[k] = tmp;
-        kk++;    if (kk == l) printf("%d %d", A[k], A[smaller]);
-        heapify(A, smaller, n);
-    }
+	printf("%s", resultc);
 }
