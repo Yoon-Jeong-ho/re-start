160a7707e43dd96cf44c3f2ef9134371dda423ad
K\aa007 <aa007878@naver.com>
2023-11-05 22:46:28 +0900

    드디어 스프링 기본 끝

---
+++ java/spring/core/src/test/java/hello/core/scope/PrototypeTest.java
@@ -0,0 +1,37 @@
+package hello.core.scope;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+public class PrototypeTest {
+    @Test
+    void prototypeBeanFind(){
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+        System.out.println("find prototype1");
+        PrototypeBean singletonBean1 = ac.getBean(PrototypeBean.class);
+        System.out.println("find prototype2");
+
+        PrototypeBean singletonBean2 = ac.getBean(PrototypeBean.class);
+        System.out.println("singletonBean1 = " + singletonBean1);
+        System.out.println("singletonBean2 = " + singletonBean2);
+        Assertions.assertThat(singletonBean1).isNotSameAs(singletonBean2);
+        ac.close();
+    }
+
+    @Scope("prototype")
+    static class PrototypeBean{
+        @PostConstruct
+        public void init(){
+            System.out.println("PrototypeBean.init");
+        }
+
+        @PreDestroy
+        public void destroy(){
+            System.out.println("PrototypeBean.destroy");        }
+    }
+}
