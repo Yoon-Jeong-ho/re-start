2f98d290211034e263029a10482c7b080eba749d
K\aa007 <aa007878@naver.com>
2023-11-06 03:05:59 +0900

    고c 과제 하나 10점 까진거 화나네

--- c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
+++ c_restart/c프로그래밍실습/c프로그래밍실습/14주차.c
@@ -11,67 +11,47 @@ strcat(a,b) : a 뒤에 b를 붙임
 strcmp(a,b) : s1의 순위
 b = atoi,l,f(a): 숫자 형에 따른 문자를 숫자로 변환
 */
-typedef struct {
-	char* name;
-	int id,test1,test2, e, m, num, result, cnt,rank;
-	float mean;
-}student;
-void input(int*, int);
-
-void output(int*, int);
+struct student {
+	char* name; // 이름 저장
+	int firstStage; // 1단계 점수 저장
+	int interview; // 면접 점수 저장
+	double sum; // 총점 저장
+};
 int main() {
-	int n, num1 = 0, num2 = 0, ** nn, m,intstore[100],*p,*q,k=0,test1pass,test2pass;
-	float* num, max = -99999;
-	student* st, ttmp;
-
-	char** c, a = 'a', b, tmp[105], minc[100] = "",str[11][105];
-	
-	scanf("%d", &n); getchar();
-	st = (student*)malloc(sizeof(student) * n);
-	if (st == NULL) return -1;
-	for (int i = 0; i < n; i++) {
-		scanf("%d %d %d", &st[i].id, &st[i].test1, &st[i].test2);
-		st[i].mean = (st[i].test1*40 + st[i].test2*60);
-		st[i].mean /= 100;
-
+	int n, m,num=0;
+	struct student* st, tmpst;
+	char tmp[100];
+	scanf("%d %d", &n, &m); getchar(); // 입력
+	st = (struct student*)malloc(sizeof(struct student) * n); // 동적할당
+	if (st == NULL) return -1; // 오류 확인
+	for (int i = 0; i < n; i++) { // 입력받기
+		scanf("%s %d %d",tmp,&st[i].firstStage,&st[i].interview); getchar(); // 입력
+		st[i].name = (char*)malloc(sizeof(char) * (strlen(tmp) + 1)); // 동적할당
+		strcpy(st[i].name, tmp); // 할당 오류 확인
+		st[i].sum = st[i].firstStage * 7 + st[i].interview * 3; // 더하기
 	}
-	test1pass = n * 0.6;
-	test2pass = n * 0.8;
 	for (int i = 0; i < n; i++) {
-		for (int j = i+1; j < n; j++) {
-			if (st[i].id > st[j].id) {
-				ttmp = st[i];
+		for (int j = i + 1; j < n; j++) {
+			if (st[i].sum < st[j].sum || (st[i].sum == st[j].sum && st[i].firstStage < st[j].firstStage) || (st[i].sum == st[j].sum && st[i].firstStage == st[j].firstStage && strcmp(st[i].name, st[j].name) > 0)) { // 조건확인
+				tmpst = st[i];// 조건에 맞으면 바꾸기
 				st[i] = st[j];
-				st[j] = ttmp;
+				st[j] = tmpst;
 			}
 		}
 	}
-	for (int i = 0; i < n; i++) {
-		num1 = 1;
-		num2 = 1;
-		for (int j = 0; j < n; j++) {
-			if (st[i].mean < st[j].mean) num1++;
-			if (st[i].test2 < st[j].test2) num2++;
-		}
-		if (num1 <= test1pass) k++;
-		st[i].rank = num1;
-		st[i].cnt = num2;
-	}
-	for (int i = 0; i < n; i++) {
-		if (st[i].rank <= test1pass) printf("%d\n", st[i].id);
-		else if (st[i].cnt <= test2pass && k < test2pass) {
-			printf("%d\n", st[i].id);
+	for (int i = 0; i < m; i++) {// m까지 출력하기
+		printf("%s %d %d %.1f\n", st[i].name, st[i].firstStage, st[i].interview, st[i].sum/10);
+		num++; // 마지막에 출력할 숫자 늘리기
+		while (i<n-1 && st[i].sum == st[i + 1].sum && st[i].firstStage == st[i + 1].firstStage) {
+			i++;// 여긴 합과 첫번째 시험이 같은 사람들 출력하는 곳
+			printf("%s %d %d %.1f\n", st[i].name, st[i].firstStage, st[i].interview, st[i].sum / 10);
+			num++;
 		}
 	}
-	free(st);
+	printf("%d", num);
+	for (int i = 0; i < n; i++) {
+		free(st[i].name);//이름 할당 해제
+	}
+	free(st); // 마지막 할당 해제
 	return 0;
-}
-void input(int* p, int n) {
-	int* q;
-	for (q = p; q < p + n; q++) scanf("%d ", q);
-}
-
-void output(int* p, int k) {
-	int* q;
-	for (q = p; q < p + k; q++) printf("%d ", *q);
 }
